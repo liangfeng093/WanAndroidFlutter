52698c743e6077787d4f5f69d815bb79732033ae
zhifeng.mo <zhifeng.mo@ecarx.com.cn>
2019-05-30 18:49:12 +0800

    git 测试

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-//    getHttp();
+    getHttp();
 
     var data = RemoteApi.getInstance().get("/article/list/0/json",Options(responseType: ResponseType.plain));
 
