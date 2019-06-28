342c38e79d83b933e1ae3df130936511511c96d4
Alex_mzf <liangfeng093@gmail.com>
2019-06-28 17:46:01 +0800

    1.添加PageView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wanandroidflutter/utils/LogUtils.dart';
 import 'package:wanandroidflutter/widget/HomeWidget.dart';
+import 'package:wanandroidflutter/widget/page_view.dart';
 
 void main() => runApp(App());
 
@@ -17,6 +19,92 @@ class AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(title: 'Flutter Demo', home: Home());
+    return MaterialApp(title: 'Flutter Demo', home: Main());
+  }
+}
+
+class Main extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MainState();
+  }
+}
+
+class MainState extends State {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 200,
+                color: Colors.grey,
+                child: Center(
+                  child: Image.asset(
+                    "assets/app_icon.png",
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+              ),
+//              Icon(Icons.add),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "首页",
+            style: TextStyle(),
+          ),
+        ),
+        bottomNavigationBar: _buildBottomNavigationBar(),
+        body: MyPageView(
+          //禁用滑动
+          physics: NeverScrollableScrollPhysics(),
+          controller: _pageControllerageController,
+          children: <Widget>[
+            Home(),
+            Text("this is 2 page"),
+          ],
+        ));
+  }
+
+  MyPageController _pageControllerageController = MyPageController();
+
+  int _currentIndex = 0;
+
+  BottomNavigationBar _buildBottomNavigationBar() {
+    BottomNavigationBar _bottomNavigationBar = BottomNavigationBar(
+      currentIndex: _currentIndex, //默认选中位置
+      onTap: (index) {
+        LogUtils.e("BottomNavigationBar===>", "index:" + index.toString());
+        switch (index) {
+          case 0:
+            {
+              _pageControllerageController.jumpToPage(index);
+              break;
+            }
+          case 1:
+            {
+              _pageControllerageController.jumpToPage(index);
+              break;
+            }
+        }
+        setState(() {
+          _currentIndex = index;
+          LogUtils.e(
+              "BottomNavigationBar===>", "_currentIndex:" + index.toString());
+        });
+      },
+      items: [
+        BottomNavigationBarItem(title: Text("首页"), icon: Icon(Icons.home)),
+        BottomNavigationBarItem(title: Text("首页"), icon: Icon(Icons.home)),
+      ],
+    );
+    return _bottomNavigationBar;
   }
 }
--- lib/widget/HomeWidget.dart
+++ lib/widget/HomeWidget.dart
@@ -23,7 +23,6 @@ class Home extends StatefulWidget {
 }
 
 class HomeStatus extends State {
-
   final String TAG = "HomeStatus";
 
   ///当前页数
@@ -78,69 +77,90 @@ class HomeStatus extends State {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-        drawer: Drawer(
-          child: Text("777"),
-        ),
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            "首页",
-            style: TextStyle(),
-          ),
-        ),
-        body: SmartRefresher(
-          enablePullDown: true,
-          enablePullUp: true,
-          header: ClassicHeader(),
-          onRefresh: () {
-            LogUtils.e("SmartRefresher", "下拉刷新");
-            _getArticleData(true);
-            //延时三秒
-            /* Future.delayed(Duration(seconds: 2), () {
+
+    return SmartRefresher(
+      enablePullDown: true,
+      enablePullUp: true,
+      header: ClassicHeader(),
+      onRefresh: () {
+        LogUtils.e("SmartRefresher", "下拉刷新");
+        _getArticleData(true);
+        //延时三秒
+        /* Future.delayed(Duration(seconds: 2), () {
               refreshController.refreshFailed();
             });*/
-          },
-          onLoading: () {
-            LogUtils.e("SmartRefresher", "上拉加载");
-            _getArticleData(false);
-          },
-          controller: refreshController,
-          child: ListView.builder(
-              itemCount: mArticles.length,
-              itemBuilder: (BuildContext context, int index) {
-                if (index == 0) {
-                  return Container(
-                    height: 200,
-                    child: BannerWidget(),
-                  );
-                }
-                if (mArticles.length > 0) {
-                  var article = mArticles[index];
-                  var time =
-                      DateTime.fromMillisecondsSinceEpoch(article.publishTime);
-                  return Container(
-                      //此处的padding实际上做出来margin的效果
-                      padding: EdgeInsets.fromLTRB(8, 5, 8, 5),
-                      child: GestureDetector(
-                        onTap: ()=>_onClickItem(index),
-                        child: ArticleItemWidget(article.title, article.author,
-                            time.toString().substring(0, 10)),
-                      ));
-                } else {
-                  return Container(
-                    height: 50,
-                    child: Center(child: Text("未获取到数据")),
-                  );
-                }
-              }),
-        ));
+      },
+      onLoading: () {
+        LogUtils.e("SmartRefresher", "上拉加载");
+        _getArticleData(false);
+      },
+      controller: refreshController,
+      child: ListView.builder(
+          itemCount: mArticles.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Container(
+                height: 200,
+                child: BannerWidget(),
+              );
+            }
+            if (mArticles.length > 0) {
+              var article = mArticles[index];
+              var time =
+                  DateTime.fromMillisecondsSinceEpoch(article.publishTime);
+              return Container(
+                  //此处的padding实际上做出来margin的效果
+                  padding: EdgeInsets.fromLTRB(8, 5, 8, 5),
+                  child: GestureDetector(
+                    onTap: () => _onClickItem(index),
+                    child: ArticleItemWidget(article.title, article.author,
+                        time.toString().substring(0, 10)),
+                  ));
+            } else {
+              return Container(
+                height: 50,
+                child: Center(child: Text("未获取到数据")),
+              );
+            }
+          }),
+    );
+  }
+
+  BottomNavigationBar _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: 1, //默认选中位置
+      onTap: (index) {
+        LogUtils.e("BottomNavigationBar===>", "index:" + index.toString());
+      },
+      items: [
+        BottomNavigationBarItem(title: Text("首页"), icon: Icon(Icons.home)),
+        BottomNavigationBarItem(title: Text("首页"), icon: Icon(Icons.home)),
+      ],
+    );
   }
 
   _onClickItem(int position) {
     LogUtils.e(TAG, "position:" + position.toString());
     var article = mArticles[position];
     LogUtils.e(TAG, "title:" + article.title);
+  }
 
+  _addDrawerChildren() {
+    List<Widget> widgets = List();
+    var drawerHeader = UserAccountsDrawerHeader(
+      accountName: Text("ID"),
+      currentAccountPicture: CircleAvatar(
+        backgroundColor: Colors.white,
+//        backgroundImage: AssetImage("assets/app_icon.png"),
+        backgroundImage: NetworkImage(
+            "https://www.wanandroid.com/resources/image/pc/logo.png",
+            scale: 15),
+//        backgroundImage: AssetImage("assets/user_icon.jpg"),
+      ),
+    );
+//    widgets.add(drawerHeader);
+    widgets.add(Stack());
+
+    return widgets;
   }
 }
